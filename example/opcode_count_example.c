dbe20771917f16a567923678ba3f71e9da2b4051
PeterRong <PeterRong96@gmail.com>
2018-09-29 14:45:15 +0800

    first apss written.
    Signed-off-by: PeterRong <PeterRong96@gmail.com>

--- example/opcode_count_example.c
+++ example/opcode_count_example.c
@@ -1,12 +1,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+int inc(int& i){ 
+	i++; 
+	return i;
+}
+
+int dec(int& i){
+	i--;
+	return i;
+}
+
 int main(int argc, char const *argv[]){
 	char* cStr = (char*) malloc(256 * sizeof(char));
 	const char* constCStr= "This is an example code used to learn llvm pass";
 	for (int i=0; i< 256; i++){
 		cStr[i] = '6';
 	}
+
 	fprintf(stderr, "%s\n", constCStr);
+
+	fprintf(stderr, "%s\n", "Testing inc function.");
+	for (int i=0; i<10; inc(i)){
+		fprintf(stderr, "%d ", i);
+	}
+	fprintf(stderr, "\n");
 	return 0;
 }
--- src/CountOpcodePass.cpp
+++ src/CountOpcodePass.cpp
@@ -1,10 +1,69 @@
-#include <llvm/Pass.h>
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include <map>
+#include <string>
 
 using namespace llvm;
 
 namespace {
 
 class CountOpcodePass: public FunctionPass{
+private:
+    std::map<std::string, int> opcodeCnt;
+public:
+    static char ID;
+    CountOpcodePass():FunctionPass(ID){ };
+    virtual bool runOnFunction(Function& function){
+        errs() << "Passing Function: " << function.getName() << "\n";
+        for (const BasicBlock& basicBlock: function){
+            for (const Instruction& instruction: basicBlock){
+                const char * opcode = instruction.getOpcodeName();
+                if (opcodeCnt.find(opcode) == opcodeCnt.end()){
+                    opcodeCnt[opcode] = 1;
+                } else {
+                    opcodeCnt[opcode] +=1;
+                }
+            }
+        }
+        for (auto entry: opcodeCnt){
+            errs() << "opcode: " << entry.first << "\t occurrence: " << entry.second << "\n";
+        }
 
+        errs() << "\n";
+        return false;
+    }
 }; // end of CountOpcodePass
 }  // end of anonymous namespace
+
+char CountOpcodePass::ID = 0;
+static RegisterPass<CountOpcodePass> X(
+        "CountOpcodePass",
+        "A pass to count the occurrence of all opcodes");
+
+static void registerCountOpcodePass(
+        const PassManagerBuilder&, legacy::PassManagerBase &PM) {
+    PM.add(new CountOpcodePass());
+}
+
+/*
+ * The following note is copied from
+ *   * https://github.com/rdadolf/clangtool/blob/master/clangtool.cpp
+ * , which helped a lot.
+ *
+ * Note: The location EP_OptimizerLast places this pass at the end of the list
+ * of *optimizations*. That means on -O0, it does not get run.
+
+ * In general, adding your pass twice will cause it to run twice, but in this
+ * particular case, the two are disjoint (EP_EnabledOnOptLevel0 only runs if
+ * you're in -O0, and EP_OptimizerLast only runs if you're not). You can check
+ *  * include/llvm/Transforms/IPO/PassManagerBuilder.h
+ *  * lib/Transforms/IPO/PassManagerBuilder.cpp
+ * for the exact behavior.
+ */
+static RegisterStandardPasses registerCountOpcodePassVarOptLast(
+        PassManagerBuilder::EP_OptimizerLast, registerCountOpcodePass);
+static RegisterStandardPasses registerCountOpcodePassVarOptLevel0(
+        PassManagerBuilder::EP_EnabledOnOptLevel0, registerCountOpcodePass);
