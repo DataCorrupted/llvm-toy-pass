a5512965358d2784967e0eda7feac84e5cd41c65
PeterRong <PeterRong96@gmail.com>
2018-10-10 19:18:02 +0800

    added a pass to examine +-*/%
    Signed-off-by: PeterRong <PeterRong96@gmail.com>

---
+++ src/OverflowingBinaryPass.cpp
@@ -0,0 +1,52 @@
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+
+using namespace llvm;
+
+namespace {
+
+class OverflowingBinaryPass: public FunctionPass{
+public:
+	static char ID;
+	OverflowingBinaryPass(): FunctionPass(ID){};
+	virtual bool runOnFunction(Function& function){
+		errs() << "Passing Function: " << function.getName() << "\n";
+		errs() << "  Opearion | hasNoUnsignedWrap | hasNoSignedWrap  \n";
+		for (const BasicBlock& basicBlock: function){
+			for (const Instruction& instruction: basicBlock){
+				if (const BinaryOperator* binInst 
+                  = dyn_cast<BinaryOperator>(&instruction)){
+                  	errs() << "|    ";
+					errs() << binInst->getOpcodeName() << "   |       ";
+					errs() << (binInst->hasNoUnsignedWrap()? "true " : "false") << "       |      ";
+					errs() << (binInst->hasNoSignedWrap()? "true " : "false") << "      |\n";
+				}
+			}
+		}
+		errs() << "\n";
+		return false;
+	}
+}; // enf of OverflowingBinaryPass
+}  // end of anonymous namespace 
+
+
+char OverflowingBinaryPass::ID = 0;
+static RegisterPass<OverflowingBinaryPass> X(
+        "OverflowingBinaryPass",
+        "A pass to examine all binary opearions");
+
+static void registerOverflowingBinaryPass(
+        const PassManagerBuilder&, legacy::PassManagerBase &PM) {
+    PM.add(new OverflowingBinaryPass());
+}
+
+static RegisterStandardPasses registerOverflowingBinaryPassVarOptLast(
+        PassManagerBuilder::EP_OptimizerLast, registerOverflowingBinaryPass);
+static RegisterStandardPasses registerOverflowingBinaryPassVarOptLevel0(
+        PassManagerBuilder::EP_EnabledOnOptLevel0, registerOverflowingBinaryPass);
